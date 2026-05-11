483ca802d09f5240f33230dee66d650c33eeb201
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-11 14:18:18 +0300

    feat: withMargin() can add margin to 1x1 all alive pattern

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,1 +1,5 @@
 export const duplicateOf = arr => arr.map(row => Array.from(row));
+
+export const withMargin = arr => {
+  return ['bbb'.split(''), 'bob'.split(''), 'bbb'.split('')];
+};
--- test/utils.test.mjs
+++ test/utils.test.mjs
@@ -3,6 +3,7 @@ import {
   patternFromString,
   rleFromString,
 } from './utils.mjs';
+import { withMargin } from '../src/utils.mjs';
 
 describe('rleFromString()', () => {
   test('can read 1x1 all dead pattern from string', () => {
@@ -20,5 +21,18 @@ describe('patternFromString()', () => {
       b
     `;
     expect(patternFromString(patternString)).toEqual([['b']]);
+  });
+});
+
+describe('withMargin()', () => {
+  test('can add margin to 1x1 all alive pattern', () => {
+    const pattern = patternFromString(`
+      o
+    `);
+    expect(withMargin(pattern)).toEqual(patternFromString(`
+      bbb
+      bob
+      bbb
+    `));
   });
 });
