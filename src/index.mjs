04256864458f573b61486e8a8f20f3a1892e4513
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-11 19:50:29 +0300

    feat: the application shows an error message when there is an error

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,7 @@
 import runApplication from './runApplication.mjs';
 
-console.log(await runApplication(process.argv.slice(2)));
+try {
+  console.log(await runApplication(process.argv.slice(2)));
+} catch (error) {
+  console.error(`Error: ${error.message}`);
+}
